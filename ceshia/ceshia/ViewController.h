96350237d3447409bc4f05efbe9b1ab3ebd49f56
FYY <941949529@qq.com>
2017-01-10 11:40:21 +0800

    初始化项目

---
+++ ceshia/ceshia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshia
+//
+//  Created by 冯亚运 on 17/1/10.
+//  Copyright © 2017年 冯亚运. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ceshia/ceshiaTests/ceshiaTests.m
@@ -0,0 +1,39 @@
+//
+//  ceshiaTests.m
+//  ceshiaTests
+//
+//  Created by 冯亚运 on 17/1/10.
+//  Copyright © 2017年 冯亚运. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ceshiaTests : XCTestCase
+
+@end
+
+@implementation ceshiaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
