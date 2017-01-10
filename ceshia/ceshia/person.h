186b0e17157af0674dc6337f68fe5028a2be4337
FYY <941949529@qq.com>
2017-01-10 11:42:59 +0800

    创建了person类

---
+++ ceshia/ceshia/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  ceshia
+//
+//  Created by 冯亚运 on 17/1/10.
+//  Copyright © 2017年 冯亚运. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ ceshia/ceshia/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  ceshia
+//
+//  Created by 冯亚运 on 17/1/10.
+//  Copyright © 2017年 冯亚运. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
